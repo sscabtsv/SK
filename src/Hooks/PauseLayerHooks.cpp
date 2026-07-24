a417f1ee4a4581dfa237ea99867562c141ed3a6c
sscabtsv <anastacioaijhenweina@gmail.com>
2026-07-25 06:51:04 +0800

    Implement custom setup for PauseLayer with button

---
+++ src/Hooks/PauseLayerHooks.cpp
@@ -0,0 +1,40 @@
+#include <Geode/modify/PauseLayer.hpp>
+#include "../UI/MainPopup.hpp"
+#include "../UI/Theme.hpp"
+
+using namespace geode::prelude;
+using namespace showcasekit;
+
+class $modify(SKPauseLayerHook, PauseLayer) {
+    void customSetup() {
+        PauseLayer::customSetup();
+
+        auto menu = this->getChildByID("left-button-menu");
+        if (!menu) return;
+
+        auto sprite = CircleButtonSprite::createWithSpriteFrameName(
+            "SK_icon.png"_spr,
+            1.f,
+            CircleBaseColor::Green,
+            CircleBaseSize::Medium
+        );
+
+        if (!sprite) {
+            sprite = CircleButtonSprite::create("GJ_infoIcon_001.png");
+        }
+
+        auto btn = CCMenuItemSpriteExtra::create(
+            sprite,
+            this,
+            menu_selector(SKPauseLayerHook::onOpenShowcaseKit)
+        );
+        btn->setID("showcasekit-button"_spr);
+
+        menu->addChild(btn);
+        menu->updateLayout();
+    }
+
+    void onOpenShowcaseKit(CCObject*) {
+        MainPopup::create()->show();
+    }
+};
